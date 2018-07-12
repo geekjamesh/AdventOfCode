56b5daf87a9e35ef1dfec5f2c1f2a685f0788830
geekjamesh <41005826+geekjamesh@users.noreply.github.com>
2018-07-12 12:55:55 +0100

    Initial Commit of completed solution and folder structure

---
+++ 2017/Java/Day1InverseCaptcha/src/main/java/uk/co/geekconsultants/advent/Day1InverseCaptcha/App.java
@@ -0,0 +1,31 @@
+package uk.co.geekconsultants.advent.Day1InverseCaptcha;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Inverse Captcha");
+        System.out.println("Please Enter the code input : ");
+        Scanner scanner = new Scanner(System.in);
+    	try {
+            String inputString = scanner.nextLine();
+            InverseCaptcha captcha = new InverseCaptcha();
+            captcha.setInput(inputString);
+            captcha.calculateCodeNextCharacter();
+            Integer result = captcha.getResult();
+            System.out.println("Part 1 Result : " + result.toString());
+            captcha.calculateCodeOppositeHalfCharacter();;
+            result = captcha.getResult();
+            System.out.println("Part 2 Result : " + result.toString());
+    	} catch (Exception e) {
+    		System.out.println("Error! : " + e.getMessage());
+    		e.printStackTrace();
+    	} finally {
+    		scanner.close();
+    	}
+    }
+    
+    
+}
---
+++ 2017/Java/Day1InverseCaptcha/src/main/java/uk/co/geekconsultants/advent/Day1InverseCaptcha/InverseCaptcha.java
@@ -0,0 +1,61 @@
+package uk.co.geekconsultants.advent.Day1InverseCaptcha;
+
+class InverseCaptcha {
+
+	private String input;
+	private Integer result;
+	
+	protected void calculateCodeNextCharacter() {
+    	result = 0;
+		
+		for(int i = 0; i < input.length(); i++) {
+			if(i == input.length()-1) {
+				if(compareDigits(i, 0)) {
+					sumDigits(0);
+				}
+			} else {
+				if(compareDigits(i, i+1)) {
+					sumDigits(i+1);
+				}
+			}
+		}
+    }
+	
+	protected void calculateCodeOppositeHalfCharacter() {
+    	result = 0;
+    	int length = input.length();
+		int midPoint = length/2;
+		for(int i = 0; i < length; i++) {
+			if(i < midPoint) {
+				if(compareDigits(i, i+midPoint)) {
+					sumDigits(i);
+				}
+			} else {
+				if(compareDigits(i, i - midPoint)) {
+					sumDigits(i);
+				}
+			}
+		}
+
+    }
+
+	private boolean compareDigits(int firstIndex, int secondIndex) {
+		return input.charAt(firstIndex) == input.charAt(secondIndex);
+	}
+	
+	private void sumDigits(int index) {
+		result += Integer.parseInt(String.valueOf(input.charAt(index)));
+	}
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	public Integer getResult() {
+		return result;
+	}
+}
